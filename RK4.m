6cd12358a5f6986ebab0308282fbf889509b0995
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-04 10:59:53 -0600

    activity 2 finish with my a2

--- RK4.m
+++ RK4.m
@@ -7,7 +7,9 @@ y(1)=y0;
 for i=1:length(x)-1
     k1=m(x(i),y(i));
     k2=m(x(i)+h/2,y(i)+k1*h/2);
-    y(i+1)=y(i)+(a1*k1+a2*k2)*h;
+    k3=m(x(i)+h/2,y(i)+k2*h/2);
+    k4=m(x(i)+h,y(i)+k3*h);
+    y(i+1)=y(i)+((1/6*k1+(1/2)*k2+(1/2)*k3+(1/6)*k4))*h;
 end
 yapprox = y;
 
