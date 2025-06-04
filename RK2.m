e14921786a445a055d75442994ed57707720fd0d
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-04 09:47:36 -0600

    Adding a conditional

--- RK2.m
+++ RK2.m
@@ -1,4 +1,9 @@
 function yapprox = RK2(m, x0, xf, y0, h, a2)
+% Eulers method: a2=0
+% Heuns method: a2=0
+% Midpoints method: a2=1
+% Ralstons method: a2=2/3
+% Ricos method a2=666
 
 % 2nd order Runge-Kutta
 
@@ -7,9 +12,12 @@ y=zeros(size(x));
 y(1)=y0;
 
 a1=1-a2;
-q=1/(2*a2);
 
-
+if a2~=0
+    q=1/(2*a2),
+else
+    q=0;
+end
 for i=1:length(x)-1
     k1=m(x(i),y(i));
     k2=m(x(i)+q*h,y(i)+q*k1*h);
