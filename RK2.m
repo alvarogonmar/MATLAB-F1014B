cc7a5ed2d4619756560b3cde3402d7cc21fd18d7
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-04 09:39:15 -0600

    function finished

--- RK2.m
+++ RK2.m
@@ -1,22 +1,22 @@
-function yapprox = RK2(m, x0, xf, h,)
+function yapprox = RK2(m, x0, xf, h, a2)
 
 % 2nd order Runge-Kutta
 
-x=x0:h:xf
-y=zeros(size(x))
-y(1)=y0
-err=zeros(size(x))
+x=x0:h:xf;
+y=zeros(size(x));
+y(1)=y0;
+err=zeros(size(x));
 
-a1=1-a2
-q=1/(2*a2)
+a1=1-a2;
+q=1/(2*a2);
 
 
 for i=1:length(x)-1
     k1=m(x(i),y(i));
     k2=m(x(i)+q*h,y(i)+q*k1*h);
     y(i+1)=y(i)+(a1*k1+a2*k2)*h;
-
-    err(i+1)=abs((yexact(x(i+1))-y(i+1))/yexact(x(i+1)))*100;
+end
+yapprox = y;
 end
 
 
