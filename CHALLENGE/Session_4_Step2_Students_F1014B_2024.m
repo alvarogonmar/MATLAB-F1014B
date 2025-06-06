d5cd3a423a55feeeddc8c4228bdda724e7db4255
alvarogonmar <alvarogonmar0@gmail.com>
2025-06-06 09:45:33 -0600

    adding step 2 session 4

---
+++ CHALLENGE/Session_4_Step2_Students_F1014B_2024.m
@@ -0,0 +1,50 @@
+%-------------------------------%
+%   Challenge Session No. 4
+%-------------------------------%
+%Rodrigo Gamboa & Francisco Montes | May 2024
+
+
+
+%--------Part 2---------% [after %Call for figure(1).]
+
+%Use [open and close] a while loop, using the zm variable vector, such that
+%you can warranty that all z values for the magnet while falling from zo to
+%JUST BEFORE passing through the coil are covered (i.e., from zm = zo to 
+%zm = 0.0162).
+
+
+%Inside the while loop, use the following (uncomment), and
+%explain what it does or will do.
+
+%pause(0.001)     %Comment
+%clf              %Comment
+    
+
+    %[x,y,phiB1,Bz]=B_due_M(zm(cc),mag,Rring);  
+
+    %What is this? Explain in full detail, take your time!
+    %___________________________________________________________
+    %___________________________________________________________
+    %___________________________________________________________
+    %___________________________________________________________
+
+%Using kinematics, the same as in last week's code actually, write an
+%expression for the zm position of the magnet, while free falling.
+
+    %[zm free fall function here]
+
+%Using kinematics, the same as in last week's code actually, write an
+%expression for the vz position of the magnet, while free falling.
+
+    %[vz free fall function here]
+
+    %[x,y,phiB2,Bz]=B_due_M(zm(cc+1),mag,Rring);
+
+    %What is this? Explain in full detail, take your time!
+    %___________________________________________________________
+    %___________________________________________________________
+    %___________________________________________________________
+    %___________________________________________________________
+
+   
+ % close the while loop
